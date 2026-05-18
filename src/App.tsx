5f7f86d13f2264ba7e84b5cb40e6c233006cf3f8
MARIA ISABEL <Mariah22-bot>
2026-05-18 17:12:35 -0300

    feat: adiciona componente de header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-error font-medium">Welcome to React with TypeScript!</h1>
+      <Header />
     </>
   )
 }
