8dbaa488dfc5388acf00f308ceb54d53942de8e7
MARIA ISABEL <Mariah22-bot>
2026-06-12 15:56:17 -0300

    feat: adicionada página de detalhes de produtos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
-import Logo from '@/assets/images/logo.png';
-import IconUser from '@/assets/images/icon-user.png';
-import IconAbout from '@/assets/images/icon-about.png';
-import IconCart from '@/assets/images/icon-cart.png';
-import { Link } from '@tanstack/react-router';
+import Logo from "@/assets/images/logo.png";
+import IconUser from "@/assets/images/icon-user.png";
+import IconAbout from "@/assets/images/icon-about.png";
+import { Link } from "@tanstack/react-router";
 
 export const Header = () => {
     return (
         <div className="relative">
-            <header className='fixed top-10 left-0 right-0 z-8 mx-8'>
-                <div className=' bg-white text-black max-w-264 mx-auto flex justify-between items-center py-3 px-4 rounded-xl mt-4'>
-                    <img src={Logo} alt="Logo SyntaxWear" className='w-25.6 md:w-28.8' />
+            <header className="fixed top-10 left-0 right-0 z-10 mx-10">
+                <div className="bg-white text-black max-w-[1320px] mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
+                    <Link to="/">
+                        <img src={Logo} alt="Logo SyntaxWear" className="w-32 md:w-36" />
+                    </Link>
 
-                    <nav className='hidden min-[768px]:block'>
-                        <ul className='flex gap-8'>
+                    <nav className="hidden md:block">
+                        <ul className="flex gap-10">
                             <li>
                                 <a href="#">Masculino</a>
                             </li>
@@ -26,28 +27,30 @@ export const Header = () => {
                     </nav>
 
                     <nav>
-                        <ul className='flex gap-3.2 md:gap-8'>
-                            <li className='hidden min-[768px]:block'>
-                                <a href="#">Nossas lojas</a>
+                        <ul className="flex gap-4 md:gap-10">
+                            <li className="hidden md:block">
+                                <a href="#">Nossas Lojas</a>
                             </li>
-                            <li className='hidden min-[768px]:block'>
+                            <li className="hidden md:block">
                                 <a href="#">Sobre</a>
                             </li>
                             <li>
-                                <Link to="/sign-in">
+                                <a href="#">
                                     <img src={IconUser} alt="Ícone de login" />
-                                </Link>
+                                </a>
                             </li>
                             <li>
-                                <a href="#"><img src={IconAbout} alt="Ícone de sobre" /></a>
+                                <a href="#">
+                                    <img src={IconAbout} alt="Ícone de sobre" />
+                                </a>
                             </li>
-                            <li>
-                                <a href="#"><img src={IconCart} alt="Ícone de carrinho" /></a>
-                            </li>
+
                         </ul>
                     </nav>
                 </div>
             </header>
         </div>
     );
-}
+};
+
+
